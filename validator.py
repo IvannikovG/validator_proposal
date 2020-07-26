8e283c1bd5c264559a48be1b9fdf8df980d6432d
georgii <georgii>
2020-07-27 00:09:52 +0200

    Add more examples

--- validator.py
+++ validator.py
@@ -73,6 +73,14 @@ schema_this_json_represents = {
 }
 
 
+def find_failed_function_indices(li):
+    return [i for i, x in enumerate(li) if not x]
+
+
+def mapper(key, value, function_list):
+    return list(map(lambda function: function(key, value), function_list))
+
+
 def get_name(item):
     return item[0]
 
@@ -81,11 +89,13 @@ def get_content(item):
     return item[1]
 
 
-def have_proper_naming(key_name, validation_name):
-    return key_name == validation_name
+def have_proper_naming(key, validation):
+    return get_name(key) == get_name(validation)
 
 
-def have_proper_type(key_content, validation_content):
+def have_proper_type(key, validation):
+    key_content = get_content(key)
+    validation_content = get_content(validation)
     return isinstance(key_content, validation_content.get('type'))
 
 
@@ -93,43 +103,63 @@ def required_key(validation_content):
     return validation_content.get('required')
 
 
-def validate_key(key, validation):
-    key_name = get_name(key)
-    validation_name = get_name(validation)
+def have_whatever_thing(key, validation):
+    if key and validation:
+        return False
 
-    key_content = get_content(key)
+
+def validate_key(key, validation):
+
+    list_of_validation_functions = [have_proper_naming, have_proper_type, have_whatever_thing]
     validation_content = get_content(validation)
 
     # Ideally i want to apply functions from array to each (key, validation) pair (as if it is map but not exactly)
     # like (pair) <= [function1, function2, ... functionN]
     # having this validation process implemented we will be just writing new validation functions
+    # See my implementation of mapper function above
+    # Further improvements to this: put validation functions as a parameter to this function and we can use this
+    # function as a common interface for each and any Yaml structure
 
-    # for this example lets hardcode some "if"s.
     required = required_key(validation_content)
     # possible to play with required condition but just bare with me, that
-    # in this examples not required are not validated for simplicity
+    # in this example not required are not validated for simplicity
     if required:
-        if have_proper_naming(key_name, validation_name):
-            have_proper_type(key_content, validation_content)
-            print('valid_key')
+        validated = mapper(*[key, validation], list_of_validation_functions)
+
+        if all(validated):
+            print('Validated all')
+
+        if not all(validated):
+            failed_function_indices = find_failed_function_indices(validated)
+            for i in failed_function_indices:
+                print(f'Failed in {list_of_validation_functions[i].__name__} function')
+                print('')
+
+    else:
+        print('Parameter was not required ...')
+        # As opposed to this scum below
+        # if have_proper_naming(key_name, validation_name):
+        #     have_proper_type(key_content, validation_content)
+        #     print('valid_key')
 
 
 def validate_against_schema(json, schema):
+    print("VALIDATING AGAINST SCHEMA")
     json_representation = json.items()
     schema_representation = schema.items()
 
     structured = list(zip(json_representation, schema_representation))
     for item in structured:
+        print(f'Validating {get_name(item)}')
         entry = item[0]
         schema = item[1]
-        print(entry, schema)
+        print('')
         validate_key(entry, schema)
 
 
-print("VALIDATING AGAINST SCHEMA")
 validate_against_schema(json_to_validate, schema_this_json_represents)
 print('')
-print('')
+
 
 sample_changeable_schema = {
     'string': {
@@ -163,7 +193,9 @@ def change_required_values(schema, list_of_values, change_to):
 updated = change_required_values(sample_changeable_schema, ['string'], False)
 
 print("UPDATING SCHEMA")
+print('was True, became False in sample_changeable_schema string')
 print(updated)
+
 
 # So in a way we can have one schema, describing some yamls batch (like for questionnaire), then
 # Validate as in validate_against_schema and change schema with different functions, e.g. change_required_values.
@@ -174,3 +206,6 @@ print(updated)
 #           2). No rewriting of functions which are used during the validation (e.g. have_proper_naming, small ones)
 #           3). Very flexible structure of applying functions during the validation ((pair) <= [f1, f2, ...]). Allows
 #               to do anything
+
+
+
